03d38b075249de65214ac1e335c82b43f18bf29b
Derek <nightwing917@hotmail.com>
2021-02-18 19:48:46 -0700

    only grabbed three DOM elements

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-console.log("Connected!");
+const inputContainer = document.getElementById('input-container');
+const countdownForm = document.getElementById('countdownForm');
+const dateEl = document.getElementById('date-picker');
