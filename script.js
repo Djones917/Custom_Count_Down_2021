0ca32a888125536df9fb898a1ba7031862921ec3
Derek <nightwing917@hotmail.com>
2021-02-21 19:25:17 -0700

    add prevent default to form

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ const dateEl = document.getElementById('date-picker');
 // Set Date Input Min With Today's Date
 const today = new Date().toISOString().split('T')[0]; 
 dateEl.setAttribute('min', today);
+
+// Update Countdown - Take Values From Form Input
+function updateCountdown(e) {
+    e.preventDefault();
+   console.log(e);
+}
+
+// Event Listeners
+countdownForm.addEventListener('submit', updateCountdown);
